b67767d66e73c91da2771fc553f61efa0d408a39
wjm-wjm <53390698+wjm-wjm@users.noreply.github.com>
2020-07-30 00:25:20 +0800

    Add files via upload

--- ba_axisangle.cpp
+++ ba_axisangle.cpp
@@ -7,15 +7,79 @@
 using namespace std;
 using namespace Eigen;
 
+class load_data
+{
+public:
+    load_data(int num_camera_parameters){
+        num_camera_parameters_ = num_camera_parameters;
+    }
+
+    double* observations()       { return observations_;                   }
+    double* parameter_cameras()  { return parameters_;                     }
+    double* parameter_points()   { return parameters_  + num_camera_parameters_ * num_cameras_; }
+
+    template<typename T>
+    void fscanf_change(FILE* fp, const char* format, T* value){
+        int flag = fscanf(fp, format, value);
+        if(flag != 1){
+            cout << "cannot scan data!" << endl;
+        }
+    }
+    
+    bool load_file(char* filename){
+        FILE *fp = fopen(filename, "r");
+        if(fp == NULL){
+            return false;
+        }
+
+        fscanf_change(fp, "%d", &num_cameras_);
+        fscanf_change(fp, "%d", &num_points_);
+        fscanf_change(fp, "%d", &num_observations_);
+
+        point_index_ = new int[num_observations_];
+        camera_index_ = new int[num_observations_];
+        observations_ = new double[2 * num_observations_];
+        num_parameters_ = num_camera_parameters_ * num_cameras_ + 3 * num_points_;
+        parameters_ = new double[num_parameters_];
+
+        for (int i = 0; i < num_observations_;i++){
+            fscanf_change(fp, "%d", point_index_ + i);
+            fscanf_change(fp, "%d", camera_index_ + i);
+            for (int j = 0; j < 2;j++){
+                fscanf_change(fp, "%lf", observations_ + 2 * i + j);
+            }
+        }
+
+        for (int i = 0; i < num_parameters_;i++){
+            fscanf_change(fp, "%lf", parameters_ + i);
+        }
+
+        return true;
+    }
+
+    int num_camera_parameters_;
+    int num_observations_;
+    int num_points_;
+    int num_cameras_;
+    int num_parameters_;
+
+    int* point_index_;
+    int* camera_index_;
+    double* observations_;
+    double* parameters_;
+};
+
 class ReprojectionError
 {
 public:
-    ReprojectionError(double camera_id, double fx, double fy, double cx, double cy, double point_id, double u, double v){
+    ReprojectionError(double camera_id, double fx, double fy, double cx, double cy, double k1, double k2, double point_id, double u, double v){
         camera_id_ = camera_id;
         fx_ = fx;
         fy_ = fy;
         cx_ = cx;
         cy_ = cy;
+        k1_ = k1; //径向畸变参数k1
+        k2_ = k2; //径向畸变参数k2
         point_id_ = point_id;
         u_ = u;
         v_ = v;
@@ -38,14 +102,126 @@ public:
         se << rou, fa;
     }
 
-    double camera_id_, fx_, fy_, cx_, cy_, point_id_, u_, v_;
+    Matrix<double,2,1> call_error(Matrix<double,3,1> P){
+        Matrix<double, 3, 1> P_ = R * P + t;
+        double P_x = P(0, 0) / P(2, 0);
+        double P_y = P(1, 0) / P(2, 0);
+        double r = P_x * P_x + P_y * P_y;
+        double distortion = 1.0 + r * (k1_ + k2_ * r);
+        double predicted_u = fx_ * P_x * distortion + cx_;
+        double predicted_v = fy_ * P_y * distortion + cy_;
+        Matrix<double, 2, 1> error;
+        error << u_ - predicted_u, v_ - predicted_v; //观测值减去预测值
+        return error;
+    }
+
+    Matrix<double,2,9> cal_FE(Matrix<double,3,1> P){
+        Matrix<double, 3, 1> P_ = R * P + t;
+        Matrix<double, 2, 3> J_eP_;
+        J_eP_ << -fx_ / P_(2, 0), 0, fx_ * P_(0, 0) / (P_(2, 0) * P_(2, 0)), 0, -fy_ / P_(2, 0), fy_ * P_(1, 0) / (P_(2, 0) * P_(2, 0));
+        Matrix<double, 3, 6> J_P_;
+        J_P_ << Matrix3d::Identity(), -skew_matrix(P_);
+        Matrix<double, 2, 6> F = J_eP_ * J_P_;
+        Matrix<double, 2, 3> E = J_eP_ * R;
+        Matrix<double, 2, 9> EF;
+        EF << F, E;
+        return EF;
+    }
+
+    double camera_id_, fx_, fy_, cx_, cy_, point_id_, u_, v_, k1_, k2_;
     Matrix<double, 6, 1> se;
     Matrix3d R;
     Matrix<double, 3, 1> t;
 };
 
-int
-main()
+class LM_SchurOptimization
+{
+public:
+    LM_SchurOptimization(int num_iterations, int num_cameras, int num_points, double lambda, double* parameter_cameras, double* parameter_points){
+        num_iterations_ = num_iterations;
+        num_cameras_ = num_cameras;
+        num_points_ = num_points;
+        lambda_ = lambda;
+        parameter_cameras_ = parameter_cameras;
+        parameter_points_ = parameter_points;
+    }
+
+    void optimize(){
+        //给H11(B)，H22(C)，H12(E)，H21(E_T)，g1(v)，g2(w)设置初始值0
+        MatrixXd B(6 * num_cameras_, 6 * num_cameras_);
+        B = MatrixXd::Zero(6 * num_cameras_, 6 * num_cameras_);
+        MatrixXd C(3 * num_points_, 3 * num_points_);
+        C = MatrixXd::Zero(3 * num_points_, 3 * num_points_);
+        MatrixXd E(6 * num_cameras_, 3 * num_points_);
+        E = MatrixXd::Zero(6 * num_cameras_, 3 * num_points_);
+        MatrixXd E_T(3 * num_points_, 6 * num_cameras_);
+        E_T = MatrixXd::Zero(3 * num_points_, 6 * num_cameras_);
+        MatrixXd v(6 * num_cameras_, 1);
+        v = MatrixXd::Zero(6 * num_cameras_, 1);
+        MatrixXd w(3 * num_points_, 1);
+        w = MatrixXd::Zero(3 * num_points_, 1);
+        MatrixXd parameter_se(num_cameras_, 6);
+        parameter_se = MatrixXd::Zero(6, num_cameras_);
+
+        for (int i = 0; i < num_iterations_;i++){
+            double error_sum = 0;
+            for (int j = 0; j < (int)terms.size(); j++)
+            {
+                int camera_id = terms[j]->camera_id_;
+                int point_id = terms[j]->point_id_;
+
+                Matrix<double, 6, 1> camera_in;
+                for (int k = 0; k < 6;k++){
+                    camera_in(k, 0) = *(parameter_cameras_ + camera_id * 9 + k);
+                }
+                Matrix<double, 3, 1> P;
+                for (int k = 0; k < 3;k++){
+                    P(k, 0) = *(parameter_points_ + point_id * 3 + k);
+                }
+
+                terms[j]->cal_Rt_se(camera_in); //计算R，t和se
+                parameter_se.col(camera_id) = terms[j]->se; //储存se
+                Matrix<double, 2, 9> EF = terms[j]->cal_FE(P); //计算E，F
+                Matrix<double, 2, 6> F = EF.block(0, 0, 2, 6);
+                Matrix<double, 2, 3> E = EF.block(0, 6, 2, 3);
+                Matrix<double, 2, 1> error = terms[j]->call_error(P); //计算误差（观测值减去预测值）
+
+                error_sum += 0.5 * error.squaredNorm();
+
+                B.block(camera_id * 6, camera_id * 6, 6, 6) += F.transpose() * F + lambda_ * MatrixXd((F.transpose() * F).diagonal().asDiagonal());
+                C.block(point_id * 3, point_id * 3, 3, 3) += E.transpose() * E + lambda_ * MatrixXd((E.transpose() * E).diagonal().asDiagonal());
+                E.block(camera_id * 6, point_id * 3, 6, 3) += F.transpose() * E;
+                E_T.block(point_id * 3, camera_id * 6, 3, 6) += E.transpose() * F;
+                v.block(camera_id * 6, 0, 6, 1) += -F.transpose() * error;
+                w.block(point_id * 3, 0, 3, 1) += -E.transpose() * error;
+            }
+
+            MatrixXd C_inverse(3 * num_points_, 3 * num_points_);
+            C_inverse = C.inverse();
+            MatrixXd delta_parameter_cameras(6 * num_cameras_, 1);
+            delta_parameter_cameras = (B - E * C_inverse * E_T).colPivHouseholderQr().solve(v - E * C_inverse * w);
+            MatrixXd delta_parameter_points(3 * num_points_, 1);
+            delta_parameter_points = C_inverse * (w - E_T * delta_parameter_cameras);
+
+            for (int j = 0; j < num_cameras_;j++){
+                parameter_se.col(j) += delta_parameter_cameras.block(j * 6, 0, 6, 1);
+            }
+        }
+    }
+
+    void add_errorterms(ReprojectionError *e)
+    {
+        terms.push_back(e);
+    }
+
+    int num_iterations_, num_cameras_, num_points_;
+    double lambda_;
+    double *parameter_cameras_;
+    double *parameter_points_;
+    vector<ReprojectionError *> terms;
+};
+
+int main()
 {
     return 0;
 }
