b95a8a2f90b5241335589d277072540cd7ae6618
wjm-wjm <53390698+wjm-wjm@users.noreply.github.com>
2020-08-11 14:05:09 +0800

    Add files via upload

--- ceres_ba.cpp
+++ ceres_ba.cpp
@@ -1,9 +1,16 @@
 #include <cmath>
 #include <cstdio>
 #include <iostream>
+#include<fstream>
+
+#include<Eigen/Core>
+#include<Eigen/Dense>
 
 #include "ceres/ceres.h"
 #include "ceres/rotation.h"
+
+using namespace Eigen;
+using namespace std;
 
 // Read a Bundle Adjustment in the Large dataset.
 class BALProblem {
@@ -136,6 +143,68 @@ struct SnavelyReprojectionError {
   double observed_y;
 };
 
+void AngleAxisRotatePoint_byhand(Matrix<double,3,1> angle_axis, double point[3], double result[3]){
+    double theta = angle_axis.norm();
+    Matrix<double, 3, 1> norm_angle_axis = angle_axis / theta;
+    Matrix<double, 3, 3> skew_norm_angle_axis;
+    skew_norm_angle_axis << 0, -norm_angle_axis(2, 0), norm_angle_axis(1, 0), norm_angle_axis(2, 0), 0, -norm_angle_axis(0, 0), -norm_angle_axis(1, 0), norm_angle_axis(0, 0), 0;
+    Matrix<double, 3, 3> R = cos(theta) * Matrix3d::Identity() + (1 - cos(theta)) * norm_angle_axis * norm_angle_axis.transpose() + sin(theta) * skew_norm_angle_axis;
+    Matrix<double, 3, 1> point_;
+    point_ << point[0], point[1], point[2];
+    Matrix<double, 3, 1> new_point_ = R * point_;
+    for (int i = 0; i < 3;i++){
+        result[i] = new_point_(i, 0);
+    }
+}
+
+typedef Map<VectorXd> VectorRef;
+typedef Map<const VectorXd> ConstVectorRef;
+
+void CameraToAngleAxisAndCenter(double* camera, double* angle_axis, double* center, BALProblem* f){
+    VectorRef angle_axis_ref(angle_axis, 3);
+    angle_axis_ref = ConstVectorRef(camera, 3);
+
+    VectorXd inverse_rotation = -angle_axis_ref;
+    AngleAxisRotatePoint_byhand(inverse_rotation, camera + 9 - 6, center);
+
+    VectorRef(center, 3) *= -1.0;  
+}
+
+void WriteToPLYFile(const char* filename, BALProblem* f){
+    //创建一个文件输出流对象，输出至文件名
+    ofstream of(filename);
+    //uchar 类型只能保存0-255的数字溢出后会从0开始继续累加
+    of << "ply"
+       << '\n' << "format ascii 1.0"
+       << '\n' << "element vertex " << f->num_cameras_ + f->num_points_
+       << '\n' << "property double x"
+       << '\n' << "property double y"
+       << '\n' << "property double z"
+       << '\n' << "property uchar red"
+       << '\n' << "property uchar green"
+       << '\n' << "property uchar blue"
+       << '\n' << "end_header" << std::endl;
+
+    double angle_axis[3];
+    double center[3];
+    for (int i = 0; i < f->num_cameras_;i++){
+        double *camera = f->mutable_cameras() + i * 9;
+        CameraToAngleAxisAndCenter(camera, angle_axis, center, f);
+        of << center[0] << ' ' << center[1] << ' ' << center[2]
+           << " 0 255 0" << '\n';
+    }
+
+    const double *points = f->mutable_points();
+    for (int i = 0; i < f->num_points_;i++){
+        const double *point = points + i * 3;
+        for (int j = 0; j < 3;j++){
+            of << *(point + j) << ' ';
+        }
+        of << "255 255 255\n";
+    }
+    of.close();
+}
+
 int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
   if (argc != 2) {
@@ -179,6 +248,14 @@ int main(int argc, char** argv) {
   ceres::Solver::Summary summary; //优化信息
   ceres::Solve(options, &problem, &summary); //求解
   std::cout << summary.FullReport() << "\n"; //输出优化的全部信息
+
+  FILE *fpp = fopen("/home/vision/Desktop/code_c_c++/my_BA/3d_point_update_ceres.txt", "w");
+  for (int i = 0; i < bal_problem.num_points_ * 3;i++){
+      fprintf(fpp, "%.16e\n", *(bal_problem.mutable_points() + i));
+  }
+  fclose(fpp);
+
+  WriteToPLYFile("/home/vision/Desktop/code_c_c++/my_BA/3d_point_update_ceres.ply", &bal_problem);
 
   return 0;
 }
